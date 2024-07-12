f812a401a81bb3c86fef562cda2c061c787e20ee
gustavo <gustavogarrido.programador@gmail.com>
2024-07-12 10:16:45 -0400

    añadido API_KEY_CRECIC

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,6 +5,7 @@ export const envs = {
 
     PORT: get("PORT").required().asPortNumber(),
     BASE_URL_CONASET: get("BASE_URL_CONASET").required().asString(),
-    X_API_KEY: get("X_API_KEY").required().asString()
+    X_API_KEY: get("X_API_KEY").required().asString(),
+    API_KEY_CRECIC: get("API_KEY_CRECIC").required().asString()
     
 }
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
 
   const configDoc = new DocumentBuilder()
     .setTitle('Next-Conaset API')
