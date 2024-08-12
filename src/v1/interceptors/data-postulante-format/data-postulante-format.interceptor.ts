458d3507e729c7d3a2e4f0d1ed40afd475a46f2a
gustavo <gustavogarrido.programador@gmail.com>
2024-08-12 18:00:27 -0400

    decodificador caracteres

--- src/v1/interceptors/data-postulante-format/data-postulante-format.interceptor.ts
+++ src/v1/interceptors/data-postulante-format/data-postulante-format.interceptor.ts
@@ -9,9 +9,19 @@ export class DataPostulanteFormatInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const request = context.switchToHttp().getRequest();
     
+    // Transformar la clase de licencia
     request.body.driver_licence_class = this.transformClassLicense(request.body.driver_licence_class);
     
+    // Decodificar los nombres
+    request.body.applicant_names = this.decodeSpecialCharacters(request.body.applicant_names);
+    request.body.applicant_last_name = this.decodeSpecialCharacters(request.body.applicant_last_name);
+    request.body.applicant_second_last_name = this.decodeSpecialCharacters(request.body.applicant_second_last_name);
+    
     this.logger.warn('Clase Modificada:', request.body.driver_licence_class);
+    this.logger.warn('Nombre Decodificado:', request.body.applicant_names);
+    this.logger.warn('Apellido Paterno Decodificado:', request.body.applicant_last_name);
+    this.logger.warn('Apellido Materno Decodificado:', request.body.applicant_second_last_name);
+    
     return next.handle();
   }
 
@@ -28,5 +38,27 @@ export class DataPostulanteFormatInterceptor implements NestInterceptor {
       default:
         return classLicense;
     }
+  }
+
+  private decodeSpecialCharacters(text: string): string {
+    // Mapeo de números a caracteres especiales
+    const mapeo = {
+      '[1]': 'ñ',
+      '[2]': 'á',
+      '[3]': 'é',
+      '[4]': 'í',
+      '[5]': 'ó',
+      '[6]': 'ú',
+      '[7]': 'ü',
+      '[8]': 'Á',
+      '[9]': 'É',
+      '[10]': 'Í',
+      '[11]': 'Ó',
+      '[12]': 'Ú',
+      '[13]': 'Ü',
+      '[14]': 'Ñ'
+    };
+
+    return text.replace(/\[\d+\]/g, (match) => mapeo[match] || match);
   }
 }
