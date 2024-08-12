c5fa557104daa8a7dad92052c41b06840e1ae763
gustavo <gustavogarrido.programador@gmail.com>
2024-08-12 19:07:02 -0400

    cambios en log

--- src/v1/interceptors/data-estado-format-petition/data-estado-format-petition.interceptor.ts
+++ src/v1/interceptors/data-estado-format-petition/data-estado-format-petition.interceptor.ts
@@ -2,19 +2,26 @@ import { CallHandler, ExecutionContext, Injectable, Logger, NestInterceptor } fr
 import { Observable } from 'rxjs';
 
 @Injectable()
-export class DataEstadoFormatPetitionInterceptor implements NestInterceptor {
+export class DataPostulanteFormatInterceptor implements NestInterceptor {
 
-  private readonly logger = new Logger(DataEstadoFormatPetitionInterceptor.name);
+  private readonly logger = new Logger(DataPostulanteFormatInterceptor.name);
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const request = context.switchToHttp().getRequest();
     
-    this.logger.warn('Revisando y Modificando clase de licencia en petición');
+    // Transformar la clase de licencia
+    request.body.driver_licence_class = this.transformClassLicense(request.body.driver_licence_class);
     
-    if (request.body.class !== null && request.body.class !== undefined) {
-      request.body.class = this.transformClassLicense(request.body.class);
-    }
-
+    // Decodificar los nombres
+    request.body.applicant_names = this.decodeSpecialCharacters(request.body.applicant_names);
+    request.body.applicant_last_name = this.decodeSpecialCharacters(request.body.applicant_last_name);
+    request.body.applicant_second_last_name = this.decodeSpecialCharacters(request.body.applicant_second_last_name);
+    
+    this.logger.warn('Clase Modificada:', request.body.driver_licence_class);
+    this.logger.warn('Nombre Decodificado:', request.body.applicant_names);
+    this.logger.warn('Apellido Paterno Decodificado:', request.body.applicant_last_name);
+    this.logger.warn('Apellido Materno Decodificado:', request.body.applicant_second_last_name);
+    
     return next.handle();
   }
 
@@ -31,5 +38,28 @@ export class DataEstadoFormatPetitionInterceptor implements NestInterceptor {
       default:
         return classLicense;
     }
+  }
+
+  private decodeSpecialCharacters(text: string): string {
+    // Mapeo de números a caracteres especiales
+    const mapeo = {
+      '[1]': 'ñ',
+      '[2]': 'á',
+      '[3]': 'é',
+      '[4]': 'í',
+      '[5]': 'ó',
+      '[6]': 'ú',
+      '[7]': 'ü',
+      '[8]': 'Á',
+      '[9]': 'É',
+      '[10]': 'Í',
+      '[11]': 'Ó',
+      '[12]': 'Ú',
+      '[13]': 'Ü',
+      '[14]': 'Ñ'
+    };
+
+    // Reemplaza los números en el texto por los caracteres correspondientes
+    return text.replace(/\[\d+\]/g, (match) => mapeo[match] || match);
   }
 }
